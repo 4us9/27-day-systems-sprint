9c9dc7b9dacac767d1b5874185108a1e646a81fb
Pang <austinpang19@gmail.com>
2026-07-31 06:53:25 -0600

    Concurrency and concurrency problem program.

---
+++ os_concepts/intro/threads.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+#include "common_threads.h"
+
+/*Concurrency: Working on many things at once in the same program*/
+
+//Modern problems of concurrency: multi-threaded programs + OS.
+volatile int counter = 0;
+int loops;
+
+void *worker(void *arg) {
+    int i;
+    for (i=0; i<loops; i++) {
+
+        counter++;
+    }
+
+    return NULL;
+
+
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+
+        fprintf(stderr, "usage: threads<value> \n");
+        exit(1);
+    }
+
+    loops = atoi(argv[1]);
+    pthreads_t p1, p2;
+
+    printf("Initial value: %d\n", counter);
+
+    //Program creates two threads
+    Pthread_create(&p1, NULL, worker, NULL); //routine called worker that increments `loops`
+    Pthread_create(&p2, NULL, worker, NULL);
+
+
+    Pthread_join(p1, NULL);
+    Pthread_join(p2, NULL);
+
+    printf("Final value: %d\n", counter);
+    return 0;
+
+}
+
+//this program does not run atomically (load value of ctr from MEM to reg, increment, store into MEM)
+//Therefore a problem with concurrency. Does not give expected value at all times. 
