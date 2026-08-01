ab5a016b922e3a3b92d8ed4a0ed243baaa68a1f7
Pang <austinpang19@gmail.com>
2026-08-01 10:54:44 -0600

    Split 1 of Karatsuba algo implemented based on 64 long number.

--- algorithms/leetcode_ds/multiplication.py
+++ algorithms/leetcode_ds/multiplication.py
@@ -1,10 +1,10 @@
 #The best case for multiply two n-digit integers is NOT n^2 time.
 
-### Karatsuba Algorithm ###
+### Karatsuba Algorithm -- Only One Karatsuba Split ###
 
 #Number 1: 3141592653589793238462643383279502884197169399375105820974944592
 #Number 2: 2718281828459045235360287471352662497757247093699959574966967627
-def multiply(a, b):
+def multiply(x, y):
     
     #Need to find base. 
     #base system is 10 and need m that is how many when in half
@@ -13,29 +13,37 @@ def multiply(a, b):
     B=10**m  
     
     #Since a and b are strings, just get the first 32 and last 32
-    print(a[:m])
-    print(b[m:])
     
+    #Step 1 & 2:
     
-    
-    #result1 += helper_multiply(a)
-    #result2 += helper_multiply(b)
-    
-    
-    #rint(result1 +result2)
+    #1st digit
+    a_str = x[:m]
+    b_str = x[m:]
 
-#def helper_multiply(a, b):
-#    if: 
-#        return
-#    else:
-        
-#        return 
+    a = int(a_str)
+    b = int(b_str)
+    
+    #2nd digit
+    c_str = y[:m]
+    d_str = y[m:]
+    
+    c = int(c_str)
+    d = int(d_str)
+    
+    #Step 3: (a+b)(c+d) - ac - bd
+    step_3 = (a+b)*(c+d) - (a*c) -(b*d)
+    
+    #Step 4: (ac)(10^2) + (Step 3)(10) + bd
+    final = (a*c)*(B**2) + step_3*(B) + b*d
+    
+    print(final)
+
     
 if __name__ == "__main__":
-    a = input("Enter first number: ")
-    b = input("Enter second number: ")
+    x = input("Enter first number: ")
+    y = input("Enter second number: ")
     
-    multiply(a, b)
+    multiply(x, y)
     
 
 '''
@@ -49,13 +57,13 @@ Step 2: Find a, b, c, d
 
 a = 8
 b = 3
-c = 5
+c = 4
 d = 6
 
 Step 3: (a+b)(c+d) - ac - bd
 
-(8+3)(5+6) - 40 - 18
-= 63
+(8+3)(4+6) - 32 - 18
+= 60
 
 Step 4: (ac)(10^2) + (Step 3)(10) + bd
 
