6da8ceb337e573ca12b530f2f56eb90ebb64775e
Pang <austinpang19@gmail.com>
2026-08-01 09:55:11 -0600

    Brainstorming divide and conquer Karatsuba algorithm.

--- algorithms/leetcode_ds/multiplication.py
+++ algorithms/leetcode_ds/multiplication.py
@@ -1,9 +1,20 @@
-
+#The best case for multiply two n-digit integres is NOT n^2 time.
 def multiply(a, b):
-    1+2
+    a = int(a)
+    b = int(b)
+    
+    a/2 #divide and conquer
+    print(a*b)
+
+def helper_multiply(a, b):
+    if: 
+    
+    else:
     
 if __name__ == "__main__":
     a = input("Enter first number: ")
     b = input("Enter second number: ")
     
     multiply(a, b)
+    
+    
