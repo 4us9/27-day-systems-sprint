c5fb7d4d751a98c23aed430770fea218015f58cc
Pang <163340552+4us9@users.noreply.github.com>
2026-08-17 06:44:22 -0600

    Fun script to print the first and last name.

---
+++ os_concepts/intro/first_last.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo -n "What is your first name? "
+
+read first
+
+echo -n "What is your last name? "
+read last
+
+echo "Thank you, we have processed your full name ($first $last). Please wait while we take you to the next page.."
